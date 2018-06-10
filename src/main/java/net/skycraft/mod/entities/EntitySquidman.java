12a8b7b283d52868ac16990c20db2d3584967d03
Indianajaune <aurelien.demont@isen.fr>
2018-06-10 16:33:35 +0200

    Updated squidman

--- src/main/java/net/skycraft/mod/entities/EntitySquidman.java
+++ src/main/java/net/skycraft/mod/entities/EntitySquidman.java
@@ -2,11 +2,13 @@ package net.skycraft.mod.entities;
 
 
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.INpc;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAIAttackMelee;
-import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -14,7 +16,7 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-public class EntitySquidman extends EntityCreature implements IMob
+public class EntitySquidman extends EntityCreature implements  INpc
 {
 
 
@@ -31,6 +33,12 @@ public class EntitySquidman extends EntityCreature implements IMob
     protected void initEntityAI() {
         this.tasks.addTask(2, new EntityAIWander(this, 0.5D));
         this.tasks.addTask(4, new EntityAIAttackMelee(this, 0.65D, true));
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(3, new EntityAIRestrictOpenDoor(this));
+        this.tasks.addTask(4, new EntityAIOpenDoor(this, true));
+        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 0.6D));
+        this.tasks.addTask(9, new EntityAIWatchClosest2(this, EntityPlayer.class, 3.0F, 1.0F));
+        this.tasks.addTask(10, new EntityAIWatchClosest(this, EntityLiving.class, 8.0F));
     }
 
     @Override
@@ -41,5 +49,16 @@ public class EntitySquidman extends EntityCreature implements IMob
     @Override
     protected boolean canDropLoot() {
         return true;
+    }
+
+
+    protected SoundEvent getHurtSound()
+    {
+        return SoundEvents.ENTITY_SQUID_HURT;
+    }
+
+    protected SoundEvent getDeathSound()
+    {
+        return SoundEvents.ENTITY_SQUID_DEATH;
     }
 }
